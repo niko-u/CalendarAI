b8cf8899bf8e3cea65c9f81b85bc7f5df9c3f72a
Anna Bettmann <annabettmann@Annas-MacBook-Pro-109.local>
2023-06-15 16:25:21 -0500

    changed url to prod

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,10 @@
 
-export const baseURL = 'http://localhost:105'
-export const prodURL = 'http://localhost:105'
+// export const baseURL = 'http://localhost:105'
+// export const prodURL = 'http://localhost:105'
+
+export const baseURL = 'https://singular-kitsune-d432a2.netlify.app'
+export const prodURL = 'https://singular-kitsune-d432a2.netlify.app'
+
 
 export const endpoints = {
     pdfgpt: {
