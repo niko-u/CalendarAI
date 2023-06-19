4446d41a793b2d756bc846b39377f07bb92baacb
niko-u <77936525+niko-u@users.noreply.github.com>
2023-06-19 11:45:01 -0700

    fixed prod url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,8 +2,8 @@
 // export const baseURL = 'http://localhost:105'
 // export const prodURL = 'http://localhost:105'
 
-export const baseURL = 'https://nu1.pythonanywhere.com/'
-export const prodURL = 'https://nu1.pythonanywhere.com/'
+export const baseURL = 'https://nu1.pythonanywhere.com'
+export const prodURL = 'https://nu1.pythonanywhere.com'
 
 
 export const endpoints = {
